8b7e9688037214a27f12628aaa3ab0c9756bdc98
Mahad Wajid <121817699+mahadwajid@users.noreply.github.com>
2023-08-31 12:13:32 +0500

    Main

--- Server/cloudinaryConfig.js
+++ Server/cloudinaryConfig.js
@@ -6,6 +6,7 @@ cloudinary.v2.config({
   cloud_name: 'dqhkcdp4o',
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true,
 });
 
 export default cloudinary;
