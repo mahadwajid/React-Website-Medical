d427dbe7d5529f1934b5078415f61cb11c07fe48
Mahad Wajid <121817699+mahadwajid@users.noreply.github.com>
2023-08-31 16:22:25 +0500

    Main

--- Server/cloudinaryConfig.js
+++ Server/cloudinaryConfig.js
@@ -2,11 +2,13 @@ import cloudinary from 'cloudinary';
 import dotenv from 'dotenv'
 dotenv.config();
 
+const api_key=process.env.CLOUDINARY_API_KEY;
+const api_secret=process.env.CLOUDINARY_API_SECRET;
 
 cloudinary.v2.config({
   cloud_name: 'dqhkcdp4o',
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  api_key,
+  api_secret,
   secure: true,
 });
 
