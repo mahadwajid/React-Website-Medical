73c1fa7e93c7f7e7c1665004a59f56ed45cf13c6
Mahad Wajid <121817699+mahadwajid@users.noreply.github.com>
2023-09-09 08:41:37 -0700

    Main

--- diabatics/src/Components/Home.js
+++ diabatics/src/Components/Home.js
@@ -62,14 +62,7 @@ function Home() {
             <img src={img3} className="d-block w-100" alt="..." />
           </div>
         </div>
-        {/* <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Next</span>
-        </button> */}
+  
       </div>
 
       <div style={{ margin: "20px 0", padding: "0 10px" }}>
