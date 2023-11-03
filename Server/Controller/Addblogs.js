b9f6457a354b3919a1a185d8bbea563d90447d11
Mahad Wajid <121817699+mahadwajid@users.noreply.github.com>
2023-11-03 13:27:21 +0500

    Update Addblogs.js

--- Server/Controller/Addblogs.js
+++ Server/Controller/Addblogs.js
@@ -2,116 +2,43 @@ import BlogModel from '../Model/Addblog.js';
 import cloudinary from '../cloudinaryConfig.js';
 
 // Create a new blog
-// export const createBlog = async (req, res) => {
-//   const { title, content, publishDate , author, authorImage, image } = req.body;
-
-//   try {
-    
-//     const imageUploadResult = await cloudinary.uploader.upload(req.files['image'][0].path, {
-//       folder: "images",
-//     });
-//     const authorImageUploadResult = await cloudinary.uploader.upload(req.files['authorImage'][0].path, {
-//       folder: "images",
-//     });
-
-//     const newBlog = new BlogModel({
-//       title,
-//       author,
-//       content,
-//       publishDateTime: new Date(publishDate),
-//       authorImage: {
-//         public_id: authorImageUploadResult.public_id,
-//         url: authorImageUploadResult.secure_url
-//       } ,
-//       image: {
-//         public_id: imageUploadResult.public_id,
-//         url: imageUploadResult.secure_url
-//       },
-     
-//     });
-
-//      const savedProduct = await newBlog.save();
-//      console.log(savedProduct);
-
-//      res.json({Response:true , message:'Added Successfully '});
-//      console.log("Product added successfully");
-//   } catch (error) {
-//   console.log(error);
-//   }
-// };
-
-
-import AWS from 'aws-sdk';
-import BlogModel from '../models/BlogModel'; // Import your BlogModel
-import { v4 as uuidv4 } from 'uuid';
-
-// Configure AWS SDK with your AWS credentials and region
-AWS.config.update({
-  accessKeyId: 'ASIAYTMTJLTCA3XJRETZ',
-  secretAccessKey: 'z2DEsSfkVJAJUE8QaVxx+2tRD/aQpkClfgJeVUlM',
-  region: 'ca-central-1', // Replace with your desired AWS region
-});
-
-const s3 = new AWS.S3();
-
-// Create a new blog
 export const createBlog = async (req, res) => {
-  const { title, content, publishDate, author, authorImage, image } = req.body;
+  const { title, content, publishDate , author, authorImage, image } = req.body;
 
   try {
-    // Generate unique filenames for images using UUID
-    const imageFileName = `images/${uuidv4()}.jpg`;
-    const authorImageFileName = `images/${uuidv4()}.jpg`;
+    
+    const imageUploadResult = await cloudinary.uploader.upload(req.files['image'][0].path, {
+      folder: "images",
+    });
+    const authorImageUploadResult = await cloudinary.uploader.upload(req.files['authorImage'][0].path, {
+      folder: "images",
+    });
 
-    // Upload images to S3 bucket
-    const imageUploadParams = {
-      Bucket: 'cyclic-wild-cummerbund-newt-ca-central-1', // Replace with your S3 bucket name
-      Key: imageFileName,
-      Body: req.files['image'][0].buffer,
-      ContentType: 'image/jpeg', // Adjust the content type based on your file type
-      ACL: 'public-read', // Set the appropriate ACL for your use case
-    };
-
-    const authorImageUploadParams = {
-      Bucket: 'cyclic-wild-cummerbund-newt-ca-central-1', // Replace with your S3 bucket name
-      Key: authorImageFileName,
-      Body: req.files['authorImage'][0].buffer,
-      ContentType: 'image/jpeg', // Adjust the content type based on your file type
-      ACL: 'public-read', // Set the appropriate ACL for your use case
-    };
-
-    await Promise.all([
-      s3.upload(imageUploadParams).promise(),
-      s3.upload(authorImageUploadParams).promise(),
-    ]);
-
-    // Create a new blog instance with S3 image URLs
     const newBlog = new BlogModel({
       title,
       author,
       content,
       publishDateTime: new Date(publishDate),
       authorImage: {
-        url: `https://cyclic-wild-cummerbund-newt-ca-central-1.s3.ca-central-1.amazonaws.com/${authorImageFileName}`,
-      },
+        public_id: authorImageUploadResult.public_id,
+        url: authorImageUploadResult.secure_url
+      } ,
       image: {
-        url: `https://cyclic-wild-cummerbund-newt-ca-central-1.s3.ca-central-1.amazonaws.com/${imageFileName}`,
+        public_id: imageUploadResult.public_id,
+        url: imageUploadResult.secure_url
       },
+     
     });
 
-    // Save the new blog to the database
-    const savedBlog = await newBlog.save();
+     const savedProduct = await newBlog.save();
+     console.log(savedProduct);
 
-    res.json({ Response: true, message: 'Added Successfully' });
-    console.log('Blog added successfully:', savedBlog);
+     res.json({Response:true , message:'Added Successfully '});
+     console.log("Product added successfully");
   } catch (error) {
-    console.error('Error adding blog:', error);
-    res.status(500).json({ Response: false, message: 'Internal Server Error' });
+  console.log(error);
   }
 };
-
-
-
 
 
 // Get all blogs
