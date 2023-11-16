6e8a8f3e6874c13bed73d56fb93b86632672e7c2
Mahad Wajid <121817699+mahadwajid@users.noreply.github.com>
2023-11-16 21:53:30 +0500

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -20,8 +20,10 @@ connection.then(() => {
   console.log("Connection Error", error);
 });
 
-// const PORT = process.env.PORT || 5000; 
-app.listen(3000);
+const PORT = process.env.PORT || 5000; 
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 app.use(cors());
 app.use(bodyParser.json({ extended: true }));
